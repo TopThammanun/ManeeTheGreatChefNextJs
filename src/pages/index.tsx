def6793521e03f191a3ffe0529e093ed7b08670d
Top <thammanun_a@cmu.ac.th>
2023-05-02 11:01:28 +0700

    init project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,6 +43,7 @@ export default function Home() {
           priority
         />
       </div>
+      <h1 className='text-3xl font-bold'>ManeeTheGreatChef</h1>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
