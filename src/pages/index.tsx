9baefb35e154ca908a4f72a56960782633510058
Top <thammanun_a@cmu.ac.th>
2023-05-02 11:24:00 +0700

    test change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,7 +43,6 @@ export default function Home() {
           priority
         />
       </div>
-      <h1 className='text-3xl font-bold'>ManeeTheGreatChef</h1>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
